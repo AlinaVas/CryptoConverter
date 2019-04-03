5b80b0ec64513222df3b8f5e03a8b251032b0f78
Alina FESYK <afesyk@e1r6p10.unit.ua>
2019-04-03 17:13:49 +0300

    Get request

--- CryptoConverter/CryptoListPresenter.swift
+++ CryptoConverter/CryptoListPresenter.swift
@@ -17,23 +17,71 @@ class CryptoListPresenter {
     
     weak var viewDelegate: CryptoListDelegate?
     
-    private var currency: [Crypto] = []
+    private var cryptoList: [Crypto] = []
     
     
+    
+    init() {
+        getCryptoList()
+    }
+    
+    //    completion: (Result<(URLResponse, Data), Failure>) -> Void
+    func getCryptoList() {
+        
+        guard let url = URL(string: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest") else {
+            //            completion(.failure(.badURLFormat))
+            print("badurl")
+            return
+        }
+        
+        guard let apiKey = ProcessInfo.processInfo.environment["API_KEY"] else {
+            //            completion(.failure(.apiNotProvided))
+            print("no api")
+            return
+        }
+        
+        let parameters = ["start" : "1",
+                          "limit" : "2",
+                          "convert" : "USD"]
+        
+        let headers = ["X-CMC_PRO_API_KEY" : apiKey]
+        
+        Alamofire.request(url, method: .get, parameters: parameters, headers: headers) .responseJSON {
+            response in
+            
+            guard response.result.isSuccess, let value = response.value else {
+                //                completion(.failure(.failure(<#T##Error#>)))
+                print("server is error \(response)")
+                return
+            }
+            //            handleSuccess(data: response.result.value)
+            print("VALUE*************************************", value)
+            //                print("RESPONSE*************************************", response)
+        }
+        
+    }
+    
+    func handleError(error: Error) {
+        
+    }
+    
+    func handleSuccess(data: Data) {
+        
+    }
     
 }
 
 extension CryptoListPresenter {
     
     func getNumberOfRows() -> Int {
-        return currency.count
+        return cryptoList.count
     }
     
     func getCryptoName(at index: Int) -> String {
-        return currency[index].name
+        return cryptoList[index].name
     }
     
     func getCryptoPriceUSD(at index: Int) -> String {
-        return String(currency[index].priceUSD)
+        return String(cryptoList[index].priceUSD)
     }
 }
--- CryptoConverter/CryptoListVC.swift
+++ CryptoConverter/CryptoListVC.swift
@@ -22,10 +22,6 @@ class CryptoListVC: UITableViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
